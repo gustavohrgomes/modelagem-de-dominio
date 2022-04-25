d32b00a00fc87cfc8333ae6651065631d97a5c8d
Gustavo Gomes <dev.gustavogomes@gmail.com>
2022-04-25 07:54:49 -0300

    Lançando eventos de dominio

---
+++ src/NerdStore.Catalogo.Domain/Events/ProdutoAbaixoEstoqueEvent.cs
@@ -0,0 +1,14 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain.Events
+{
+    public class ProdutoAbaixoEstoqueEvent : DomainEvent
+    {
+        public ProdutoAbaixoEstoqueEvent(Guid aggragateId, int quantidadeRestante) : base(aggragateId)
+        {
+            QuantidadeRestante = quantidadeRestante;
+        }
+
+        public int QuantidadeRestante { get; private set; }
+    }
+}
---
+++ src/NerdStore.Core/Messages/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace NerdStore.Core.Messages
+{
+    public abstract class Message
+    {
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+
+        public string MessageType { get; protected set; }
+        public Guid AggregateId { get; protected set; }
+    }
+}

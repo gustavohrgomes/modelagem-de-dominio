c794b227a8ed10fdb3b732cd705e131a88e4caff
gustavohrgomes <dev.gustavogomes@gmail.com>
2022-05-04 07:07:43 -0300

    Command e Command Handler

---
+++ src/NerdStore.Core/Messages/Command.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+using MediatR;
+
+namespace NerdStore.Core.Messages
+{
+    public abstract class Command : Message, IRequest<bool>
+    {
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
